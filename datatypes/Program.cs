25eb9f904aa78a4e85180eb9c63b985ba0114472
gabriel <gabrielcmoris@gmail.com>
2023-09-01 16:17:22 +0200

    data types

---
+++ datatypes/Program.cs
@@ -0,0 +1,21 @@
+﻿// strings
+
+string firstname ="Juan";
+Console.WriteLine("Hello " + firstname);
+
+// data type conversion
+
+// int i;
+// i="Hello Wrong"; // this data type is not expected, it is declared as integer;
+
+double k=3.5;
+int i =(int)k;
+string str = i.ToString();
+int j= int.Parse(str);
+int l =j+20;
+
+
+Console.WriteLine(k);
+Console.WriteLine(i);
+Console.WriteLine(str);
+Console.WriteLine(l);
